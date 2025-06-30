ea9f1e7d16d4a1e286cb420fc42f65d52f265ae3
Akindipe Muheez <akindipemuheez@gmail.com>
2025-06-30 13:23:25 +0100

    Property fixed

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,22 @@
+-- Total Bookings By Each User
+
+SELECT
+    u.id AS user_id,
+    u.first_name,
+    COUNT(b.id) AS total_bookings
+FROM user u
+LEFT JOIN booking b ON u.id = b.user_id
+GROUP BY u.id, u.first_name;
+
+-- Show most booked property
+SELECT 
+    b.property_id,
+    p.name AS property_name,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM 
+    booking b
+JOIN 
+    property p ON b.property_id = p.id
+GROUP BY 
+    b.property_id, p.name;
