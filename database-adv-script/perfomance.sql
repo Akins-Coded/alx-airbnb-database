5fa1e2fa5adb886f5bbfaf3e554a3691c885d2da
Akindipe Muheez <akindipemuheez@gmail.com>
2025-06-30 14:25:02 +0100

    performance final commit

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,55 +1,80 @@
 -- database: alx-airbnb-database
 
-SELECT 
+SELECT
     b.id AS booking_id,
-    b.start_time,
-    b.end_time,
-    b.total_price AS booking_total,
-    b.status AS booking_status,
-
+    b.created_at,
+    
     u.id AS user_id,
     u.first_name,
     u.last_name,
-    u.email,
-    u.phone_number,
-    u.role AS user_role,
-
+    u.email AS user_email,
+    
     p.id AS property_id,
     p.name AS property_name,
-    p.description AS property_description,
-    p.location,
-    p.pricepernight,
-
+    p.location AS address,
+    
     pay.id AS payment_id,
+    pay.amount,
+    pay.created_at AS payment_date
+
+FROM 
+    booking b,
+    user u,
+    property p,
+    payment pay
+
+WHERE
+    b.user_id = u.id
+    AND b.property_id = p.id
+    AND pay.booking_id = b.id
+LIMIT 1000;
+
+EXPLAIN ANALYZE
+
+SELECT
+    b.id AS booking_id,
+    b.created_at,
+    
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email AS user_email,
+    
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location AS address,
+    
+    pay.id AS payment_id,
+    pay.amount,
+    pay.created_at AS payment_date
+
+FROM 
+    booking b,
+    user u,
+    property p,
+    payment pay
+
+WHERE
+    b.user_id = u.id
+    AND b.property_id = p.id
+    AND pay.booking_id = b.id
+LIMIT 1000;
+
+-- OPTIMIZED QUERY
+SELECT
+    b.id AS booking_id,
+    b.created_at,
+    u.first_name,
+    u.last_name,
+    u.email AS user_email,
+    p.name AS property_name,
+    p.location AS property_location,
     pay.amount AS payment_amount,
-    pay.payment_method,
     pay.created_at AS payment_date
 
 FROM booking b
 JOIN user u ON b.user_id = u.id
 JOIN property p ON b.property_id = p.id
-LEFT JOIN payment pay ON pay.booking_id = b.id;
+LEFT JOIN payment pay ON pay.booking_id = b.id
 
--- optimized query for performance
-SELECT 
-    b.id AS booking_id,
-    b.start_time,
-    b.end_time,
-    b.total_price,
-    b.status,
-
-    u.first_name,
-    u.last_name,
-    u.email,
-
-    p.name AS property_name,
-    p.location,
-    p.pricepernight,
-
-    pay.amount AS payment_amount,
-    pay.payment_method
-
-FROM booking b
-INNER JOIN user u ON b.user_id = u.id
-INNER JOIN property p ON b.property_id = p.id
-LEFT JOIN payment pay ON pay.booking_id = b.id;
+LIMIT 1000;
